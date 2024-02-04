ac55cb5be5420dc35dfda33baa152fbea0568851
Алексей Савельев <aliakseisavelyey@gmail.com>
2024-02-04 11:51:45 +0300

    feat: add main section

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ VANTA.BIRDS({
   mouseControls: true,
   touchControls: true,
   gyroControls: false,
-  minHeight: 500.00,
   minWidth: 200.00,
   scale: 1.00,
-  scaleMobile: 1.00
+  scaleMobile: 1.00,
+  backgroundColor:0xffffff,
 })
